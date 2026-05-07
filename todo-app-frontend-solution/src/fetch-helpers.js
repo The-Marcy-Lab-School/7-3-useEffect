3746f14034fb9b2225914315e99c037c0ce8a8a6
benspector-mls <ben@marcylabschool.org>
2026-05-07 12:00:21 -0400

    adding in response ok check

--- todo-app-frontend-solution/src/fetch-helpers.js
+++ todo-app-frontend-solution/src/fetch-helpers.js
@@ -1,6 +1,7 @@
 export const fetchAllTodos = async () => {
   try {
     const response = await fetch('/api/todos');
+    if (!response.ok) return Error(`Error ${response.status} - ${response.statusText}`)
     const data = await response.json();
     return { data, error: null };
   } catch (error) {
@@ -15,6 +16,7 @@ export const createTodo = async (title) => {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ title }),
     });
+    if (!response.ok) return Error(`Error ${response.status} - ${response.statusText}`)
     const data = await response.json();
     return { data, error: null };
   } catch (error) {
